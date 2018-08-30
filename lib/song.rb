1b9a7c31995d43eddc55f4df0ebfec59a9204edc
celestinosalim <celestino.salim@gmail.com>
2018-08-30 16:39:01 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+
+class Song
+
+attr_accessor :name, :artist
+
+@@all = []
+
+def initialize(name)
+  @name = name
+  @@all << self
+end
+
+def artist_name(name)
+  self.artist = name
+end
+
+
+
+end #class end
