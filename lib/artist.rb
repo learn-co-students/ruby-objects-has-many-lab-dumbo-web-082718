44935c0b4768d6c689f3c7bf7135b3c5f1201b83
GavIn Sokolof <sokolofg@gmail.com>
2018-08-31 16:28:15 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+require 'pry'
+class Artist
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+      @songs << song
+      song.artist = self
+  end
+
+  def add_song_by_name(name)
+    name = Song.new(name)
+    @songs << name
+    name.artist = self
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+
+  attr_accessor :name, :artist, :songs
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    if self.artist
+      return self.artist.name
+    else
+      return nil
+    end
+  end
+  @@all = []
+
+end
