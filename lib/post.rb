52dfc5064be6d38a3f048a3a5ae65555bbd6d332
Santiago Sendon <santiagosendon92@gmail.com>
2018-09-01 15:42:57 -0400

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,22 @@
+class Post
+  attr_accessor :title, :author
 
-  class Post
+  @@all = []
 
-    attr_accessor :title, :author
-    def initialize(title)
-      @title = title
-    end
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
 
-    def author_name
-      if @author == nil
-        return nil
-      else
-        @author.name
-      end
+  def self.all
+    @@all
+  end
+
+  def author_name
+    if self.author
+      self.author.name
+    else
+      nil
     end
   end
+end
